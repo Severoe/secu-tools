8d58fa20a2635d92281ee4d3cb23eb0aaf33e15c
Qiyang He <qiyangh@andrew.cmu.edu>
2018-05-29 19:01:38 -0400

    fix mismatched flag in build-in profile

--- Probacs/security-tool-server/makeDatabase.py
+++ Probacs/security-tool-server/makeDatabase.py
@@ -26,7 +26,7 @@ profile_data = [
 	{'uploader':'jeffery','upload_time':'2006-10-25 14:30:59','name':'max_speed','target_os':'linux','compiler':'gcc','version':'6.7','flag':['-O1 -O0','-O0 -O2']},
 	{'uploader':'jeffery','upload_time':'2006-10-25 14:30:59','name':'max_optimization','target_os':'linux','compiler':'gcc','version':'6.7','flag':['-Wall','-O3']},
 	{'uploader':'Dave','upload_time':'2006-10-25 14:30:59','name':'max_speed','target_os':'win','compiler':'vs','version':'6.7','flag':['/Wall','/O3']},
-	{'uploader':'Dave','upload_time':'2006-10-25 14:30:59','name':'max_optimization','target_os':'win','compiler':'vs','version':'6.7','flag':['/Wextra /O1','/Wextra /Od']},
+	{'uploader':'Dave','upload_time':'2006-10-25 14:30:59','name':'max_optimization','target_os':'win','compiler':'vs','version':'6.7','flag':['/WX /O1','/WX /Od']},
 
 ]
 
