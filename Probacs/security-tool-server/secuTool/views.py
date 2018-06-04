45359950987d7941734deaf83190a20bf309491b
Shipei Zhou <shipeiz@andrew.cmu.edu>
2018-06-04 11:11:46 -0400

    moved compilation module inside views.py, can generate notice on completion of compilation of a single executable

--- Probacs/security-tool-server/secuTool/views.py
+++ Probacs/security-tool-server/secuTool/views.py
@@ -327,5 +327,60 @@ def compile(task_id, target_os, compiler, version, src_path, dest_folder, invoke
 	#test:
 	# tmp = Task.objects.get(task_id=task_info['task_id'],flag=task_info['flag'])
 	#print(tmp.exename)
+    invoke_format = invoke_format.replace("_", " ")
+    flag_list = flags.replace("_", " ").split(",")
+
+    task_info = {"task_id": task_id,
+    			"target_os": target_os,
+    			"compiler": compiler,
+    			"version": version,
+    			"src_path": src_path,
+    			"dest_folder": dest_folder}
+
+    if os.name == 'nt':
+        delimit = "\\"
+    else:
+        delimit = "/"
+
+    name, extension = src_path.split(delimit)[-1].split('.')
+
+    if dest_folder[-1] == delimit:
+        dest_folder = dest_folder[0:-1]
+
+    if os.path.exists(dest_folder) and not os.path.isdir(dest_folder):
+        sys.stderr.write("Output directory already exists!\n")
+        sys.stderr.flush()
+        exit(-1)
+
+    if not os.path.exists(dest_folder):
+        os.mkdir(dest_folder)
+
+    dest_folder += delimit
+    log_filename = dest_folder + name + ".log"
+    log_file = open(log_filename, "w")
+
+    print("compilation begins...")
+        
+    cnt = 0
+    for flag in flag_list:
+        cnt += 1
+        exename = dest_folder + name + "_%d_%s"%(cnt, flag.replace(" ", "_"))
+        logline = "%s\t%s"%(exename, flag)
+
+        command = invoke_format.replace("flags", flag).replace("source", src_path).replace("exename", exename).split(" ")
+        print(command)
+        compilation = Popen(command, stdout=PIPE, stderr=PIPE)
+        out, err = compilation.communicate()
+        log_file.write("%s, %s, %s\n"%(logline, out, err))
+        
+        # execute callback to notice the completion of a single compilation
+        task_info['out'] = out
+        task_info['err'] = err
+        task_info['exename'] = exename
+        task_info['flag'] = flag
+        on_complete(task_info)
+
+    log_file.close()
+    print("compilation done!")
 
 
