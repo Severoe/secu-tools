445a37983851a44671e48323990a9ed77fb225d3
zhftsinghua <zhftsinghua@icloud.com>
2018-07-08 23:29:47 -0400

    fix the onload edit_flags

--- Probacs/host-server/secuTool/static/secuTool/preview_functions.js
+++ Probacs/host-server/secuTool/static/secuTool/preview_functions.js
@@ -90,7 +90,7 @@ function add_row() {
 				'<td class="os">' + $(this).closest('tr').find('td.os').text() + '</td>' +
 				'<td class="compiler">' + $(this).closest('tr').find('td.compiler').text() + '</td>' +
 				'<td class="profile">' + $(this).closest('tr').find('td.profile').text() + '</td>' +
-				'<td class="flag" contenteditable="true">' + $(this).closest('tr').find('td.flag').text() + ", " + flags + '</td>' +
+				'<td class="new_flag" contenteditable="true">' + $(this).closest('tr').find('td.flag').text() + ", " + flags + '</td>' +
 				'<td class="username">' + $(this).closest('tr').find('td.username').text() + '</td>' +
 				'<td class="tags">' + $(this).closest('tr').find('td.tags').text() + '</td>' +
 				'<td><button class="btn btn-light btn-sm" onclick="delete_row(\'r' + button_id + '\')">delete</button></td>'
@@ -112,6 +112,7 @@ function add_row() {
 	$('#groups').empty()
 	$('#group').empty()
 	$('#group-handin').empty()
+	edit_flags('.new_flag')
 }
 
 function delete_row(row) {
@@ -124,12 +125,11 @@ function delete_row(row) {
 	document.getElementById("log").innerHTML += logtext
 }
 
-function edit_flags() {
-	$('.flag').focus(function () {
+function edit_flags(flag) {
+	$(flag).focus(function () {
 		original = $(this).text()
-		console.log(original)
 	});
-	$('.flag').blur(function () {
+	$(flag).blur(function () {
 		updated = $(this).text()
 		if (original != updated) {
 			id = $(this).closest('tr').find('td.id').text()
@@ -196,7 +196,7 @@ function main_page() {
 
 function onload_wrapper() {
 	display_flags()
-	edit_flags()
+	edit_flags('.flag')
 }
 
 window.onload = onload_wrapper;
