5d54ab2174918a9f2164cb7b6581b662b67601e8
Qiyang He <qiyangh@andrew.cmu.edu>
2018-07-13 13:24:31 -0400

    start coding cmdline interface

---
+++ Probacs/host-server/probacs.py
@@ -0,0 +1,23 @@
+
+def handin_task(srcfile, taskfile):
+
+
+
+if __name__ == '__main__':
+	'''
+	1. compile with src and task file 
+		- python probacs.py compile src.c task.txt
+			- return task-id numbers, username, tags
+
+	2. tracking progress when compiling  -> 
+
+		- (download by id)
+
+		- 
+	2. 
+	'''
+	## 
+
+	## 
+
+
--- Probacs/host-server/rebuildDB.sh
+++ Probacs/host-server/rebuildDB.sh
@@ -6,3 +6,4 @@ python manage.py makemigrations
 python manage.py migrate
 python makeDatabase.py
 
+
