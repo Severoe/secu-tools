3a1703a13e01215747304a13d77bf824027939b6
Qiyang He <qiyangh@andrew.cmu.edu>
2018-07-27 17:06:50 -0400

    adding multi-file support

--- Probacs/platform-server/pfServer/views.py
+++ Probacs/platform-server/pfServer/views.py
@@ -45,7 +45,7 @@ def execute(request):
 		tar = r'"C:\Program Files (x86)\GnuWin32\bin\tar.exe"'
 	else:
 		tar = "tar"
-	os.system(tar+" xvf "+ task_dir+filename+" "+task_dir)
+	os.system(tar+" xvf "+ task_dir+filename+" "+task_dir+delimit+".")
 
 	print(request.FILES['file'])
 
