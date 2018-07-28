e0ee65d7f2c64cbc5e3606d7f824904e78c1c7db
Qiyang He <qiyangh@andrew.cmu.edu>
2018-07-28 15:53:51 -0400

    fixing out-err problem on win

--- Probacs/platform-server/make_compilation.py
+++ Probacs/platform-server/make_compilation.py
@@ -82,11 +82,12 @@ def compile(task_id, target_os, compiler, version, name, dest_folder, invoke_for
         exefname = name.split(".")[0] + "_%d_%s"%(cnt, flag.replace(" ", "_"))
         exename = exe_path_prefix+"secu_compile_platform" + delimit+exefname
         if os.name == 'nt':
+            exefname = exefname.replace("/","-")
             exename = exename.replace("/","-")
         logline = "%s\t%s"%(exename, flag)
 
         command = invoke_format.replace("flags", flag).replace("exename", exename).split(" ")
-        print(command)
+        # print(command)
         compilation = Popen(command, cwd = src_dir,stdout=PIPE, stderr=PIPE)
         out, err = compilation.communicate()
         #check file existense
