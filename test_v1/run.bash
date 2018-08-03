8b3264eafae0e237d92458d29d08f56e1f0b9642
zhftsinghua <zhftsinghua@icloud.com>
2018-08-03 17:26:34 -0400

    remove test v1 and v2

--- test_v1/run.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/qiyangh/Desktop/docs/docs/security-tool-server
-python manage.py runserver 0.0.0.0:8000
-
--- test_v2/win-secu-server/test.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-command = "python make_compilation.py hello.c test"
-print(command)
-os.system(command)
